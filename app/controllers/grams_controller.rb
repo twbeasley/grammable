c86708f3cab5949dcff6d7761a55a5b5e9456108
Tristan Beasley <twbeasley13@gmail.com>
2018-08-13 20:22:19 -0400

    secure edit and destroy

--- app/controllers/grams_controller.rb
+++ app/controllers/grams_controller.rb
@@ -1,16 +1,16 @@
 class GramsController < ApplicationController
-  before_action :authenticate_user!, only [:new, :create]
+  before_action :authenticate_user!, only [:new, :create, :edit, :update, :destroy]
   def destroy
     @gram = Gram.find_by_id(params[:id])
     return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.destroy
     redirect_to root_path
   end
   
   def update
     @gram = Gram.find_by_id(params[:id])
-    return render_not_found if @gram.blank?
-    
+    return render_not_found(:forbidden) if @gram.user != current_user
     @gram.update_attributes(gram_params)
     if @gram.valid?
     redirect_to  root_path
@@ -20,8 +20,10 @@ class GramsController < ApplicationController
   
   def edit
     @gram = Gram.find_by_id(params[:id])
-    if @gram.blank?
-      return render_not_found if @gram.blank?
+    return render_not_found if @gram.blank?
+    return render_not_found(:forbidden) if @gram.user =! current_user
+    end
+  end
   def index
   end
   
